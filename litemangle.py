6839c026a5c1c23e4cb91f54b6594a6ef421212e
Martin J White <mwhite@berkeley.edu>
2013-12-12 15:44:38 -0800

    Added reference to larger package.

--- litemangle.py
+++ litemangle.py
@@ -13,7 +13,8 @@ class LiteMangle:
     LiteMangle:
     A Python class to implement some very basic mangle routines which
     allow us to manipulate a mangle mask.
-    This is a subset of the more complicated Python-based Mangle package,
+    This is a subset of the more complicated Python-based Mangle package
+    (https://github.com/mollyswanson/mangle/commits/master),
     though it contains more methods than strictly necessary.
     The class is initialized with 1 argument, the file name of an
     ascii string (in Mangle polygon format) containing the mask.
